479ccb8cca9cf9ce02ea19763951d99995cfa2f8
Dylan Alvarez <helllo5d123@gmail.com>
2018-08-26 21:39:52 -0300

    Add multiple endpoints and html rendering

--- server/app/app.py
+++ server/app/app.py
@@ -1,39 +1,16 @@
-import os
 from flask import Flask
+from flask import request
+from flask import render_template
 
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'login!'
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    return app
-
-
-def run():
-    app = create_app()
-
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    run()
+        return 'login?'
