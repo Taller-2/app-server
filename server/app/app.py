9e3f85f57c2238130914bfa2ddd095c76fc2013f
Dylan Alvarez <helllo5d123@gmail.com>
2018-08-29 00:24:03 -0300

    Make linters a little bit happier

--- server/app/app.py
+++ server/app/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, render_template
-from flask_pymongo import PyMongo
 import json
 import datetime
+from flask import Flask, request, jsonify, render_template
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
 
 class JSONEncoder(json.JSONEncoder):
     # extend json encoder for compatibility with mongoDB
@@ -13,14 +14,17 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/myDatabase'
 app.json_encoder = JSONEncoder
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # mongoDB test
 @app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
@@ -32,13 +36,19 @@ def user():
     data = request.get_json()
     if request.method == 'POST':
         if not all(key in data.keys() for key in ['name', 'email']):
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+            return jsonify(
+                {'ok': False, 'message': 'Bad request parameters!'}
+            ), 400
         mongo.db.users.insert_one(data)
-        return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
+        return jsonify(
+            {'ok': True, 'message': 'User created successfully!'}
+        ), 200
 
     if request.method == 'DELETE':
         if 'email' not in data:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+            return jsonify(
+                {'ok': False, 'message': 'Bad request parameters!'}
+            ), 400
         db_response = mongo.db.users.delete_one({'email': data['email']})
         if db_response.deleted_count == 1:
             response = {'ok': True, 'message': 'record deleted'}
@@ -48,7 +58,9 @@ def user():
 
     if request.method == 'PATCH':
         if not data.get('query', {}) != {}:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+            return jsonify(
+                {'ok': False, 'message': 'Bad request parameters!'}
+            ), 400
         mongo.db.users.update_one(
             data['query'],
             {'$set': data.get('payload', {})}
