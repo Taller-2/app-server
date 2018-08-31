b892f50f2feda7b83a64dc522653b8c039b854e1
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-08-31 01:50:00 -0300

    Failed commit. Fix pylint issues but wildcard still using  wildcard imports

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 os.environ.update({'ROOT_PATH': ROOT_PATH})
 sys.path.append(os.path.join(ROOT_PATH, 'server'))
 
-from logger import logger
+import logger
 from app import app
 from app import users
 
--- server/app/users.py
+++ server/app/users.py
@@ -2,7 +2,7 @@
 import os
 from flask import request, jsonify
 from app import app, mongo
-from server.logger import logger
+import logger
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
