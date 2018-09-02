99b20650ae62561991d98ac3aa471635b2d3c8e8
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-02 20:43:13 -0300

    Renombre de funcion

--- server/routes/root.py
+++ server/routes/root.py
@@ -4,8 +4,6 @@ from server.index import app
 
 
 @app.route('/')
-# Esta funcion se llama index2 porque si se llamara
-# index rompe el codigo por dos funciones del mismo nombre
-def index2():
+def index():
     """ static files serve """
     return send_from_directory('template', 'index.html')
