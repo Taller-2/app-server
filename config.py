71461e237b602640f4481bdbfc5bb2c25d5f23bf
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-02 22:22:34 -0300

    update config

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ import flask
 
 
 class Config(flask.Config):
-    PORT = 4000
     DEBUG = os.environ.get('ENV') == 'development'
-    MONGO_URI = os.environ.get('DB', "mongodb://mongodb:27017/todoDev")
+    MONGO_URI = os.environ.get('DB', "mongodb://mongodb:27017/")
