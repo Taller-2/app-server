fc439d7586fe413ebf7f9bf1d8d1652c360c21a0
Dylan Alvarez <helllo5d123@gmail.com>
2018-09-06 20:29:03 -0300

    Add /ping

--- server/__init__.py
+++ server/__init__.py
@@ -25,8 +25,11 @@ def create_app():
 
     from server.routes.root import EXAMPLE_BP
     from server.routes.users import MONGO_TEST
+    from server.routes.ping import PING_BP
+
     app.register_blueprint(EXAMPLE_BP)
     app.register_blueprint(MONGO_TEST)
+    app.register_blueprint(PING_BP)
 
     # use the modified encoder class to handle ObjectId and Datetime object
     # while jsonifying the response
