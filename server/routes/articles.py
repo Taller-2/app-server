965dbadc0f7d0adb906b278a7dc48d9c066bc134
Dylan Alvarez <helllo5d123@gmail.com>
2018-09-09 20:03:41 -0300

    Show all articles when GETting /article

--- server/routes/articles.py
+++ server/routes/articles.py
@@ -1,5 +1,8 @@
-from flask import request, jsonify, Blueprint
+import flask
+from bson.json_util import dumps
+from flask import request, Blueprint
 
+from server.libs.mongo import mongo
 from server.model import crud
 
 ARTICLES_BP = Blueprint('articles', __name__, url_prefix='/article')
@@ -8,7 +11,11 @@ ARTICLES_BP = Blueprint('articles', __name__, url_prefix='/article')
 @ARTICLES_BP.route('/', methods=['GET', 'POST'])
 def article():
     if request.method == 'GET':
-        return jsonify(crud.get(request.args, 'articles')), 200
+        return flask.Response(
+            response=dumps(mongo.db['articles'].find(request.args)),
+            status=200,
+            mimetype='application/json'
+        )
     return crud.post(
         request.get_json(),
         'articles',
