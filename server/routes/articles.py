168c748f59841b7c0348107343efc6012a95e2b8
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-22 17:08:39 -0300

    Separo get y post de article en funciones distintas

--- server/routes/articles.py
+++ server/routes/articles.py
@@ -4,6 +4,7 @@ from flask import request, Blueprint, Response, jsonify
 from server.decorators.login_required import login_required
 from server.libs.mongo import mongo
 from server.model import crud
+from server.utils import response
 
 ARTICLES_BP = Blueprint('articles', __name__, url_prefix='/article')
 
@@ -12,19 +13,22 @@ ARTICLES_BP = Blueprint('articles', __name__, url_prefix='/article')
 def delete_article(_id):
     deleted = crud.delete({'id': _id}, 'articles').deleted_count == 1
     if not deleted:
-        return jsonify({'ok': False}), 500
-    return jsonify({'ok': True, 'message': 'Deleted'}), 200
+        return response(message=f"Error deleting article {_id}", ok=False), 500
+    return response(message=f"Successfully deleted article {_id}", ok=True), 200
 
 
-@ARTICLES_BP.route('/', methods=['GET', 'POST'])
+@ARTICLES_BP.route('/', methods=['GET'])
+def get_article():
+    return Response(
+        response=dumps(mongo.db['articles'].find(request.args)),
+        status=200,
+        mimetype='application/json'
+    )
+
+
 @login_required
-def article():
-    if request.method == 'GET':
-        return Response(
-            response=dumps(mongo.db['articles'].find(request.args)),
-            status=200,
-            mimetype='application/json'
-        )
+@ARTICLES_BP.route('/', methods=['POST'])
+def post_article():
     return crud.post(
         request.get_json(),
         'articles',
