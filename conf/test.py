3363ab3c354067ce6044970cf5c55907ccb6f113
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-25 18:24:11 -0300

    Modify docstrings to English
    you win @dylan

--- conf/test.py
+++ conf/test.py
@@ -3,8 +3,8 @@ from conf.local import Config as BaseConfig
 
 
 class Config(BaseConfig):
-    """Clase de conf para tests. Hereda de la conf base (en local.py), y overridea
-    los campos necesarios para correr los tests
+    """Tests config class. Overrides base config class attributes necessary to run tests
+    without affecting the real server's state
     """
     MONGO_URI = os.environ.get('DB', "mongodb://localhost:27017/testing")
 
