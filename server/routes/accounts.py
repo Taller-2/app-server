bd901e7e96c4d27cbddbcb35afabebd630369b63
Dylan Alvarez <helllo5d123@gmail.com>
2018-10-05 22:20:02 -0300

    Add update_current_account route

--- server/routes/accounts.py
+++ server/routes/accounts.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import request, Blueprint, jsonify
 
 from server.decorators.login_required import login_required
 from server.model.account import Account
+from server.utils import response
 
 ACCOUNTS_BP = Blueprint('accounts', __name__, url_prefix='/account')
 
@@ -10,3 +11,19 @@ ACCOUNTS_BP = Blueprint('accounts', __name__, url_prefix='/account')
 @login_required
 def current_account():
     return jsonify({"ok": True, "data": Account.current().to_json()}), 200
+
+
+@ACCOUNTS_BP.route('/current/', methods=['PATCH'])
+@login_required
+def update_current_account():
+    body = request.get_json(silent=True)
+
+    if not body:
+        return response("Invalid or empty request body", ok=False), 400
+
+    account = Account.current()
+    account['email'] = body['email']
+    account['name'] = body['name']
+    account.save()
+
+    return response(message="Successfully updated current account!", ok=True)
