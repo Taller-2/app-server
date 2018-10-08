b5fc94537222da715ecc4868d1e494a5c84a6c42
Dylan Alvarez <dylanalvarez1995@gmail.com>
2018-10-08 16:01:11 -0300

    User profile modification (#24)
    * Add update_current_account route
    
    * Add update_current_account test

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,4 +1,20 @@
-def test_account(client):
+import json
+
+
+def test_get_current_account(client):
     keys = ['user_id', 'email', 'name', '_id']
     resp = client.get('/account/current/')
     assert all(key in resp.json['data'] for key in keys)
+
+
+def test_update_current_account(client):
+    email = 'test@test.com'
+    name = 'test'
+    client.patch('/account/current/', data=json.dumps({
+        'email': email,
+        'name': name,
+    }), content_type='application/json')
+
+    resp = client.get('/account/current/')
+    assert resp.json['data']['email'] == email
+    assert resp.json['data']['name'] == name
