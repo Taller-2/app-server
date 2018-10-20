89c984167851058bd25a85e267afcb214dd4419f
sebastian <sebastian.e.blanco@gmail.com>
2018-10-20 01:00:47 -0300

    add implementation of data logginf after posting and saving an article

--- server/model/article.py
+++ server/model/article.py
@@ -1,5 +1,8 @@
 from typing import Optional
+import time
+import datetime
 from server.model.base import Model
+from server.model.article_statistics import ArticleStatistics
 
 
 class Article(Model):
@@ -17,3 +20,20 @@ class Article(Model):
         'payment_methods': Optional[list],
         'tags': Optional[list]
     }
+
+    def save_statistic(self, action):
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        body = {}
+        for key in self._data.keys():
+            if key not in ArticleStatistics.schema.keys():
+                continue
+            body[key] = self._data[key]
+        body['action'] = action
+        body['timestamp'] = st
+        article_statistics = ArticleStatistics(body)
+        article_statistics.save()
+
+    def save(self):
+        self.save_statistic('post')
+        return super(Article, self).save()
