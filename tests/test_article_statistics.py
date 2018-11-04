eca5451cda0ee6548ce7dab6200e785d71a136e8
sebastian <sebastian.e.blanco@gmail.com>
2018-11-04 01:39:49 -0300

    testing posted geted and patched articles

--- tests/test_article_statistics.py
+++ tests/test_article_statistics.py
@@ -35,8 +35,8 @@ def test_schema_of_logeed_data_of_inserted_article(client):
     assert resp.json['data'][0]['longitude'] == 0
 
 
-def test_message_i(client):
-    client.post('/article/', data=json.dumps({
+def test_schema_of_updated_article(client):
+    post = client.post('/article/', data=json.dumps({
         'price': 1,
         'name': 'nombre',
         'description': 'desc',
@@ -45,15 +45,53 @@ def test_message_i(client):
         'longitude': 0,
         'user': 'fake_user'
     }), content_type='application/json')
-    resp = client.get('/article_stats/')
-    assert resp.json['ok']
-    assert len(resp.json['data']) == 1
-    assert resp.json['data'][0]['name'] == 'nombre'
-    assert resp.json['data'][0]['action'] == 'post'
-    assert resp.json['data'][0]['description'] == 'desc'
-    assert resp.json['data'][0]['price'] == 1
-    assert resp.json['data'][0]['latitude'] == 0
-    assert resp.json['data'][0]['longitude'] == 0
+
+    _id = post.json['_id']
+    patch = client.patch('/article/', data=json.dumps({
+        '_id': _id,
+        'price': 100000,
+        'name': 'other name'
+    }), content_type='application/json')
+    assert patch.status_code == 200
+
+    get = client.get('/article_stats/')
+    assert get.json['ok']
+    print(get.json['data'])
+    assert len(get.json['data']) == 2
+    assert get.json['data'][1]['name'] == 'other name'
+    assert get.json['data'][1]['action'] == 'update'
+    assert get.json['data'][1]['description'] == 'desc'
+    assert get.json['data'][1]['price'] == 100000
+    assert get.json['data'][1]['latitude'] == 0
+    assert get.json['data'][1]['longitude'] == 0
+
+
+def test_schema_of_consulted_articles(client):
+    post = client.post('/article/', data=json.dumps({
+        'price': 1,
+        'name': 'name',
+        'description': 'desc',
+        'available_units': 11,
+        'latitude': 0,
+        'longitude': 0,
+        'user': 'fake_user'
+    }), content_type='application/json')
+
+    assert post.status_code == 200
+
+    get_articles = client.get('/article/')
+    assert get_articles.status_code == 200
+    assert len(get_articles.json['data']) == 1
+
+    get = client.get('/article_stats/')
+    assert get.json['ok']
+    assert len(get.json['data']) == 2
+    assert get.json['data'][1]['name'] == 'name'
+    assert get.json['data'][1]['action'] == 'get'
+    assert get.json['data'][1]['description'] == 'desc'
+    assert get.json['data'][1]['price'] == 1
+    assert get.json['data'][1]['latitude'] == 0
+    assert get.json['data'][1]['longitude'] == 0
 
 
 def test_logged_data_of_posted_article_filter_bad_param(client):
