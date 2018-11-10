f10e597e16db72e0722a7ca817b80769185fc30a
Dylan Alvarez <helllo5d123@gmail.com>
2018-11-10 20:14:47 -0300

    Replace Account's serialization's score field with method call so that it's not optional

--- server/model/account.py
+++ server/model/account.py
@@ -14,6 +14,12 @@ class Account(Model):
         'profile_picture_url': Optional[str]
     }
 
+    def to_json(self):
+        return {
+            **super(Account, self).to_json(),
+            **{'score': self.score()}
+        }
+
     @classmethod
     def current(cls) -> 'Account':
         return cls.get_many(user_id=user_id())[0]
