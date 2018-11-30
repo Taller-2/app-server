a32ceeaa0f931de8c7cdf9bcd566728667ca63a4
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-11-30 17:38:42 -0300

    Add logging in Firebase module

--- server/libs/firebase.py
+++ server/libs/firebase.py
@@ -1,3 +1,5 @@
+from server.logger.logger import get_root_logger
+
 import firebase_admin
 from firebase_admin import credentials, messaging
 from flask import current_app
@@ -5,6 +7,8 @@ from flask import current_app
 from server.model.account import Account
 
 API_KEY_PATH = "apikey.json"
+
+logger = get_root_logger("Firebase")
 
 
 class FirebaseMessage:
@@ -27,12 +31,16 @@ class FirebaseMessage:
             },
             token=registration_token,
         )
+        logger.info("Sending Firebase message: "
+                    "title: %s, message %s, token %s",
+                    self.title, self.message, registration_token)
 
         messaging.send(firebase_message)
 
     @staticmethod
     def init_firebase():
         api_key = current_app.config['FIREBASE_API_KEY']
+        logger.info("Initializing Firebase client. API KEY: %s", api_key)
         with open(API_KEY_PATH, mode='w') as f:
             f.write(api_key)
 
